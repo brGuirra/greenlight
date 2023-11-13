26dbff9aa7ebab0489b04ac9c1e40fa535a90662
brGuirra <br.guirra@gmail.com>
2023-11-13 14:33:43 -0300

    feat: add cors middleware

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -175,3 +175,11 @@ func (app *application) requirePermission(code string, next http.HandlerFunc) ht
 
 	return app.requireActivateUser(fn)
 }
+
+func (app *application) enableCORS(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+
+		next.ServeHTTP(w, r)
+	})
+}
